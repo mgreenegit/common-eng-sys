b35fedfb200fb38c8628e94ffe055fafb3e5d49c
mgreenegit <michael.greene@microsoft.com>
2024-10-04 08:59:14 -0500

    Manually tested on dev box

--- Tasks/dsc/runDSC.ps1
+++ Tasks/dsc/runDSC.ps1
@@ -1,24 +1,24 @@
 param(
-    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [Parameter(ValueFromPipeline = $true)]
     [string] $configurationPath,
-    [Parameter(Mandatory=$false)]
-    [string] $inlineConfiguration
+    [Parameter()]
+    $inlineConfiguration
 )
 
 # Perform download of DSC from GitHub
 function Invoke-DSCDownload {
     # Download latest release from GitHub
     $project = 'PowerShell/DSC'
-    $arch = "x86_64-pc-windows-msvc"
+    $arch = 'x86_64-pc-windows-msvc'
     $restParameters = @{
         SslProtocol = 'Tls13'
-        Headers = @{'X-GitHub-Api-Version'= '2022-11-28'}
+        Headers     = @{'X-GitHub-Api-Version' = '2022-11-28' }
     }
     # Call GitHub public API to get latest release URL
     $tags = Invoke-RestMethod -Uri "https://api.github.com/repos/$project/tags" @restParameters
-    $releaseDownloadUrl = Invoke-RestMethod -Uri "https://api.github.com/repos/$project/releases/tags/$($tags[0].name)" @restParameters | % assets | ? name -match $arch | % browser_download_url
+    $releaseDownloadUrl = Invoke-RestMethod -Uri "https://api.github.com/repos/$project/releases/tags/$($tags[0].name)" @restParameters | ForEach-Object assets | Where-Object name -Match $arch | ForEach-Object browser_download_url
     Invoke-WebRequest -Uri $releaseDownloadUrl -UseBasicParsing -OutFile "$env:temp\DSC.zip" | Out-Null
-    return $(join-path $env:temp 'DSC.zip')
+    return $(Join-Path $env:temp 'DSC.zip')
 }
 
 # Determine the install location based on user context
@@ -58,22 +58,31 @@ function Install-DSC {
 # Determine if script is running as a user or as local system
 function Get-UserContext {
     $userContext = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-    return $userContext
+    if ('NT AUTHORITY\SYSTEM' -eq $userContext) {
+        $pathType = 'system'
+    }
+    else {
+        $pathType = 'user'
+    }
+    return $pathType
 }
 
 # Test if DSC is installed, given the user context
 function Test-DSCInstalled {
-    $userContext = Get-UserContext
-    if ('NT AUTHORITY\SYSTEM' -eq $userContext) {
-        $pathType = 'system'
-    } else {
-        $pathType = 'user'
-    }
-    $installLocation = Get-InstallLocation -path $pathType
-    if (Test-Path $installLocation) {
+    # first test if it is available in PATH, since it could have been installed from the store
+    if ($null -ne $(Get-Command 'dsc' -ErrorAction Ignore)) {
         return $true
-    } else {
-        return $false
+    }
+    else {
+        # next try file path
+        $pathType = Get-UserContext
+        $installLocation = Get-InstallLocation -path $pathType
+        if (Test-Path (Join-Path $installLocation 'dsc.exe')) {
+            return $true
+        }
+        else {
+            return $false
+        }
     }
 }
 
@@ -84,11 +93,12 @@ function Get-PathType {
     )
     if ([uri]::IsWellFormedUriString($path, [urikind]::Absolute)) {
         return 'uri'
-    } elseif (Test-Path $path -ErrorAction Ignore) {
+    }
+    elseif (Test-Path $path -ErrorAction Ignore) {
         return 'file'
     }
     else {
-        throw "Configuration path provided as input is neither a URI nor a file path that exists on this machine."
+        throw 'Configuration path provided as input is neither a URI nor a file path that exists on this machine.'
     }
 }
 
@@ -99,27 +109,28 @@ if (-not (Test-DSCInstalled)) {
     Install-DSC -path (Get-UserContext)
 }
 
-
-$userContext = Get-UserContext
-if ('NT AUTHORITY\SYSTEM' -eq $userContext) {
-    $pathType = 'system'
-} else {
-    $pathType = 'user'
-}
 # Get path to dsc.exe
-$dsc = Join-Path (Get-InstallLocation -path $pathType ) 'dsc.exe'
+$dsc = Get-Command 'dsc' -ErrorAction Ignore | ForEach-Object Source
+if ($null -eq $dsc) {
+    $dsc = Join-Path (Get-InstallLocation -Path (Get-UserContext)) 'dsc.exe'
+}
+
+# Make sure inline content is string
+if ('string' -ne $inlineConfiguration.gettype().name) { $inlineConfiguration = $inlineConfiguration | Out-String }
 
 # Run DSC
-if ( 'inline' -ne $configurationPath -and (-not [string]::IsNullorEmpty($configurationPath)) ) {
+if (-not [string]::IsNullorEmpty($configurationPath) ) {
     $pathType = Get-PathType -path $configurationPath
     switch ($pathType) {
-        'uri' { $configuration = Invoke-WebRequest -Uri $configurationPath -UseBasicParsing | % content }
+        'uri' { $configuration = Invoke-WebRequest -Uri $configurationPath -UseBasicParsing | ForEach-Object content }
         'file' { $configuration = Get-Content $configurationPath }
     }
-} elseif ('inline' -eq $configurationPath -and (-not [string]::IsNullorEmpty($inlineConfiguration)))  {
+}
+elseif (-not [string]::IsNullorEmpty($inlineConfiguration)) {
     $configuration = $inlineConfiguration
-} else {
-    throw "No configuration provided"
+}
+else {
+    throw 'No configuration provided'
 }
 
 $configuration | & $dsc config set
